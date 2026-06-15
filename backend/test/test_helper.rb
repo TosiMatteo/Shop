a0c66e677ec7903846bbc1b5758f3aaff56a1f2a
TosiMatteo <mat.tosi@hotmail.com>
2026-06-15 15:48:51 +0200

    Refactor `test_helper.rb`: configure SimpleCov with parallel tests and custom worker handling

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
 require 'simplecov'
+
+SimpleCov.merge_timeout 3600
 
 SimpleCov.start 'rails' do
   add_filter '/bin/'
@@ -22,10 +22,23 @@ SimpleCov.start 'rails' do
   add_group 'Models', 'app/models'
 end
 
+require_relative "../config/environment"
+require "rails/test_help"
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    parallel_workers = ENV.fetch("PARALLEL_WORKERS", "1")
+    parallel_workers = parallel_workers == "number_of_processors" ? :number_of_processors : parallel_workers.to_i
+    parallelize(workers: parallel_workers)
+
+    parallelize_setup do |worker|
+      SimpleCov.command_name "Minitest-#{worker}"
+    end
+
+    parallelize_teardown do |_worker|
+      SimpleCov.result
+    end
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
