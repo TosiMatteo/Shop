09778febb2c21bbbca0247e2ac80cba94f63ff6a
TosiMatteo <mat.tosi@hotmail.com>
2026-09-04 10:37:19 +0200

    Update Pagy initializer: rename `options` to uppercase `OPTIONS` for consistency with library updates

--- backend/config/initializers/pagy.rb
+++ backend/config/initializers/pagy.rb
@@ -10,12 +10,11 @@ require "pagy"
   # Add your global options below. They will be applied globally.
   # For example:
   #
-  Pagy.options[:limit] = 12               # Limit the items per page
-  Pagy.options[:limit_extra] = true
-  Pagy.options[:limit_max] = 36
-  Pagy.options[:client_max_limit] = 100   # The client can request a limit up to 100
-  Pagy.options[:max_pages] = 200          # Allow only 200 pages
-# Pagy.options[:jsonapi] = true           # Use JSON:API compliant URLs
+  Pagy::OPTIONS[:limit] = 12               # Limit the items per page
+  Pagy::OPTIONS[:limit_extra] = true
+  Pagy::OPTIONS[:limit_max] = 36
+  Pagy::OPTIONS[:max_limit] = 100          # The client can request a limit up to 100
+  # Pagy::OPTIONS[:jsonapi] = true           # Use JSON:API compliant URLs
 
 ############ JavaScript ####################################################################
 # See https://ddnexus.github.io/pagy/resources/javascript/ for details.
