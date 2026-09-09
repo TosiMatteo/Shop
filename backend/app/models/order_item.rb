28bf75056c73d5b4548134979e62d5005776ae3f
TosiMatteo <mat.tosi@hotmail.com>
2026-09-09 15:36:30 +0200

    Add `sync_order_total` callbacks to update order total on order item changes

--- backend/app/models/order_item.rb
+++ backend/app/models/order_item.rb
@@ -10,9 +10,17 @@ class OrderItem < ApplicationRecord
   # Capture product price at order time if not provided.
   before_validation :set_unit_price_from_product, on: :create
 
+  after_save :sync_order_total
+  after_destroy :sync_order_total, unless: -> { destroyed_by_association }
+
   private
   # Default unit price from associated product.
   def set_unit_price_from_product
     self.unit_price ||= product.price if product
+  end
+
+  # Recompute the parent order total from its lines.
+  def sync_order_total
+    order&.recalculate_total!
   end
 end
